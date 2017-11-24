a973bf3338a634f3ef2b645407092420008e6556
Mishin870 <polsit@yandex.ru>
2017-11-25 02:47:16 +0300

    MultiChannel parser

--- src/main/java/com/xenoseus/miduino/Main.java
+++ src/main/java/com/xenoseus/miduino/Main.java
@@ -1,14 +1,15 @@
 package com.xenoseus.miduino;
 
-import com.xenoseus.miduino.arduino.ArduinoTimeLine;
 import com.xenoseus.miduino.arduino.Frequencies;
 import com.xenoseus.miduino.notes.Note;
 import com.xenoseus.miduino.notes.NoteUtils;
 import com.xenoseus.miduino.notes.TimeLine;
+import com.xenoseus.miduino.raw.RawTimeLine;
 import org.apache.log4j.Logger;
 
 import javax.sound.midi.*;
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
@@ -27,7 +28,9 @@ public class Main {
 
 	public static void main(String[] args) throws InvalidMidiDataException, IOException {
 		//входные параметры
-		String fileName = "mk.mid";
+		String fileName = "Unity.mid";
+		int[] tracksToParse = new int[] {0, 2};
+
 
 		log.info("starting");
 		Sequence sequence = MidiSystem.getSequence(new File(fileName));
@@ -36,92 +39,95 @@ public class Main {
 
 		int trackNumber = 0;
 		for (Track track : sequence.getTracks()) {
-			float currentTimeCoefficient = 1.0f;
-			Note[] notes = new Note[300];
-			TimeLine timeLine = new TimeLine();
-
-			log.info("track " + trackNumber + "; size = " + track.size());
-
-			for (int i = 0; i < track.size(); i++) {
-				MidiEvent midiEvent = track.get(i);
-				MidiMessage midiMessage = midiEvent.getMessage();
-				if (midiMessage instanceof ShortMessage) {
-					ShortMessage sm = (ShortMessage) midiMessage;
-					if (sm.getCommand() == NOTE_ON || sm.getCommand() == NOTE_OFF) {
-						int key = sm.getData1();
-						int octave = key / 12;
-						int note = key % 12;
-						int velocity = sm.getData2();
-						if ((velocity != 0) && (sm.getCommand() != NOTE_OFF)) {
-							//note start
-							if (notes[key] != null) {
-								log.warn("notes[key] not null, but new started: " + notes[key]);
-							}
-							notes[key] = new Note(note, octave, midiEvent.getTick(), velocity, key);
-						} else {
-							//note end
-							if (notes[key] == null) {
-								log.warn("notes[key] is null, but note ends, key = " + key);
-							} else {
-								notes[key].setDuration((int) ((midiEvent.getTick() - notes[key].getTick())
-										* currentTimeCoefficient));
-
-								timeLine.addNote(notes[key]);
-								notes[key] = null;
-							}
-						}
-					} else {
-						log.warn(String.format("unknown command: %s at tick %d",
-								sm.getCommand(),
-								midiEvent.getTick()
-						));
-					}
-				} else if (midiMessage instanceof MetaMessage) {
-					MetaMessage metaMessage = (MetaMessage) midiMessage;
-					if (metaMessage.getType() == SET_TEMPO) {
-						long tempo = getLongFromBytes(metaMessage.getMessage(), 3);
-						currentTimeCoefficient = NoteUtils.BPMToTimeCoefficient(NoteUtils.TempoToBPM(tempo));
-					} else if (metaMessage.getType() == END_OF_TRACK) {
-						log.info(String.format("end of track in meta message at tick %d", midiEvent.getTick()));
-					} else if (metaMessage.getType() == TRACK_NAME) {
-						log.info(String.format("track name: %s", new String(metaMessage.getMessage())));
-					} else {
-						log.warn(String.format(
-								"unknown meta message %d at tick: %d",
-								metaMessage.getType(),
-								midiEvent.getTick()
-						));
-					}
-				} else if (midiMessage.getClass().getName().contains("Track$ImmutableEndOfTrack")) {
-					log.info(String.format("end of track message at tick %d", midiEvent.getTick()));
-				} else {
-					log.warn(String.format("unknown message: %s at tick: %d",
-							midiMessage.getClass(),
-							midiEvent.getTick()
-					));
+			boolean doParse = false;
+			for (int x : tracksToParse) {
+				if (x == trackNumber) {
+					doParse = true;
+					break;
 				}
 			}
-			timeLines.add(timeLine);
+
+			if (doParse) {
+				float currentTimeCoefficient = 1.0f;
+				Note[] notes = new Note[300];
+				TimeLine timeLine = new TimeLine();
+
+				log.info("track " + trackNumber + "; size = " + track.size());
+
+				for (int i = 0; i < track.size(); i++) {
+					MidiEvent midiEvent = track.get(i);
+					MidiMessage midiMessage = midiEvent.getMessage();
+					if (midiMessage instanceof ShortMessage) {
+						ShortMessage sm = (ShortMessage) midiMessage;
+						if (sm.getCommand() == NOTE_ON || sm.getCommand() == NOTE_OFF) {
+							int key = sm.getData1();
+							int octave = key / 12;
+							int note = key % 12;
+							int velocity = sm.getData2();
+							if ((velocity != 0) && (sm.getCommand() != NOTE_OFF)) {
+								//note start
+								if (notes[key] != null) {
+									log.warn("notes[key] not null, but new started: " + notes[key]);
+								}
+								notes[key] = new Note(note, octave, midiEvent.getTick(), velocity, key);
+							} else {
+								//note end
+								if (notes[key] == null) {
+									log.warn("notes[key] is null, but note ends, key = " + key);
+								} else {
+									notes[key].setDuration((int) ((midiEvent.getTick() - notes[key].getTick())
+											* currentTimeCoefficient));
+
+									timeLine.addNote(notes[key]);
+									notes[key] = null;
+								}
+							}
+						} else {
+							log.warn(String.format("unknown command: %s at tick %d",
+									sm.getCommand(),
+									midiEvent.getTick()
+							));
+						}
+					} else if (midiMessage instanceof MetaMessage) {
+						MetaMessage metaMessage = (MetaMessage) midiMessage;
+						if (metaMessage.getType() == SET_TEMPO) {
+							long tempo = getLongFromBytes(metaMessage.getMessage(), 3);
+							currentTimeCoefficient = NoteUtils.BPMToTimeCoefficient(NoteUtils.TempoToBPM(tempo));
+						} else if (metaMessage.getType() == END_OF_TRACK) {
+							log.info(String.format("end of track in meta message at tick %d", midiEvent.getTick()));
+						} else if (metaMessage.getType() == TRACK_NAME) {
+							log.info(String.format("track name: %s", new String(metaMessage.getMessage())));
+						} else {
+							log.warn(String.format(
+									"unknown meta message %d at tick: %d",
+									metaMessage.getType(),
+									midiEvent.getTick()
+							));
+						}
+					} else if (midiMessage.getClass().getName().contains("Track$ImmutableEndOfTrack")) {
+						log.info(String.format("end of track message at tick %d", midiEvent.getTick()));
+					} else {
+						log.warn(String.format("unknown message: %s at tick: %d",
+								midiMessage.getClass(),
+								midiEvent.getTick()
+						));
+					}
+				}
+				timeLines.add(timeLine);
+			}
 			trackNumber++;
 		}
 
-		/*//сортируем и парсим линию на каналы
-		timeLine.sortLine();
-		ArrayList<TimeLine> parsedTimeLines = timeLine.channelsToTimeLine(timeLine.parseChannels());
+		RawTimeLine rawTimeLine = RawTimeLine.fromTimeLines(timeLines);
 
-		//конструируем код для первого канала
 		StringBuilder finalCode = new StringBuilder();
 
 		Frequencies frequencies = new Frequencies(0);
 		finalCode.append(frequencies.getCode());
 
-		ArduinoTimeLine arduinoTimeLine = new ArduinoTimeLine(
-				parsedTimeLines.get(0),
-				"song",
-				NoteUtils.BPMToTimeCoefficient(120));
-		finalCode.append(arduinoTimeLine.getCode());
+		finalCode.append(rawTimeLine.getCode());
 
-		log.info(finalCode.toString());*/
+		log.info(finalCode.toString());
 	}
 
 	/**
--- src/main/java/com/xenoseus/miduino/raw/RawTimeLine.java
+++ src/main/java/com/xenoseus/miduino/raw/RawTimeLine.java
@@ -1,19 +1,23 @@
 package com.xenoseus.miduino.raw;
 
+import com.xenoseus.miduino.arduino.ICoder;
 import com.xenoseus.miduino.notes.Note;
 import com.xenoseus.miduino.notes.TimeLine;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 
 /**
  * Линия времени для сырых событий (RawEvent)
  */
-public class RawTimeLine {
+public class RawTimeLine implements ICoder {
+	private static final String[] NOTES = {"C", "CS", "D", "DS", "E", "F", "FS", "G", "GS", "A", "AS", "B"};
 	private TreeMap<Long, RawEvent> events;
 
 	public RawTimeLine() {
-		this.events = new TreeMap<Long, RawEvent>();
+		this.events = new TreeMap<>();
 	}
 
 	/**
@@ -48,6 +52,164 @@ public class RawTimeLine {
 		} else {
 			events.put(longVal, event);
 		}
+	}
+
+	//====================CODER=======================
+
+	/**
+	 * Получить строку для команды beep()
+	 * @param note название ноты
+	 * @param duration длительность ноты
+	 */
+	private String getBeepCmd(String note, long duration) {
+		return String.format("\tbeep(%s, %d);", note, (long) (duration));
+	}
+
+	/**
+	 * Получить строку для команды beep2()
+	 * @param note название ноты
+	 * @param note2 название второй ноты
+	 * @param duration длительность ноты
+	 */
+	private String getBeep2Cmd(String note, String note2, long duration) {
+		return String.format("\tbeep2(%s, %s, %d);", note, note2, (long) (duration));
+	}
+
+	/**
+	 * Получить строку для команды beep3()
+	 * @param note название ноты
+	 * @param note2 название второй ноты
+	 * @param note3 название третье йноты
+	 * @param duration длительность ноты
+	 */
+	private String getBeep3Cmd(String note, String note2, String note3, long duration) {
+		return String.format("\tbeep3(%s, %s, %s, %d);", note, note2, note3, (long) (duration));
+	}
+
+	/**
+	 * Получить строку для команды beep4()
+	 * @param note название ноты
+	 * @param note2 название второй ноты
+	 * @param note3 название третьей ноты
+	 * @param note4 название четвертой ноты
+	 * @param duration длительность ноты
+	 */
+	private String getBeep4Cmd(String note, String note2, String note3, String note4, long duration) {
+		return String.format("\tbeep4(%s, %s, %s, %s, %d);", note, note2, note3, note4, (long) (duration));
+	}
+
+	/**
+	 * Получить строку для команды beep5()
+	 * @param note название ноты
+	 * @param note2 название второй ноты
+	 * @param note3 название третьей ноты
+	 * @param note4 название четвертой ноты
+	 * @param note5 название пятой ноты
+	 * @param duration длительность ноты
+	 */
+	private String getBeep5Cmd(String note, String note2, String note3, String note4, String note5, long duration) {
+		return String.format("\tbeep5(%s, %s, %s, %s, %s, %d);", note, note2, note3, note4, note5, (long) (duration));
+	}
+
+	/**
+	 * Получить строчку для команды delay()
+	 * @param delay задержка
+	 */
+	private String getDelayCmd(long delay) {
+		return String.format("\tdelay(%d);", (long) (delay));
+	}
+
+	/**
+	 * Получить полную ноту на основании её полного идентификатора
+	 * @param key идентификатор НОТЫ
+	 */
+	private String getNoteName(Integer key) {
+		int val = key.intValue();
+		int octave = val / 12;
+		int note = val % 12;
+		return String.format("%s%d",
+				NOTES[note],
+				octave
+		);
+	}
+
+	/**
+	 * Добавить команду в текст программы на основе окружения
+	 * @param stringBuilder текст программы
+	 * @param eventTick тик текущего события
+	 * @param currentTick тик предыдущего события (добавляем команды прошлого события после того, как достигли нового)
+	 * @param currentNotes текущие включенные ноты (прошлого события)
+	 */
+	private void appendCommand(StringBuilder stringBuilder, long eventTick, long currentTick, ArrayList<Integer> currentNotes) {
+		int prevNotesSize = currentNotes.size();
+		if (prevNotesSize == 0) {
+			stringBuilder.append(getDelayCmd(eventTick - currentTick)).append("\n");
+		} else {
+			if (prevNotesSize == 1) {
+				stringBuilder.append(getBeepCmd(getNoteName(currentNotes.get(0)), eventTick - currentTick)).append("\n");
+			} else if (prevNotesSize == 2) {
+				stringBuilder.append(getBeep2Cmd(getNoteName(currentNotes.get(0)),
+						getNoteName(currentNotes.get(1)),
+						eventTick - currentTick)).append("\n");
+			} else if (prevNotesSize == 3) {
+				stringBuilder.append(getBeep3Cmd(getNoteName(currentNotes.get(0)),
+						getNoteName(currentNotes.get(1)),
+						getNoteName(currentNotes.get(2)),
+						eventTick - currentTick)).append("\n");
+			} else if (prevNotesSize == 4) {
+				stringBuilder.append(getBeep4Cmd(getNoteName(currentNotes.get(0)),
+						getNoteName(currentNotes.get(1)),
+						getNoteName(currentNotes.get(2)),
+						getNoteName(currentNotes.get(3)),
+						eventTick - currentTick)).append("\n");
+			} else if (prevNotesSize == 5) {
+				stringBuilder.append(getBeep5Cmd(getNoteName(currentNotes.get(0)),
+						getNoteName(currentNotes.get(1)),
+						getNoteName(currentNotes.get(2)),
+						getNoteName(currentNotes.get(3)),
+						getNoteName(currentNotes.get(4)),
+						eventTick - currentTick)).append("\n");
+			} else {
+				stringBuilder.append(getBeep5Cmd(getNoteName(currentNotes.get(0)),
+						getNoteName(currentNotes.get(1)),
+						getNoteName(currentNotes.get(2)),
+						getNoteName(currentNotes.get(3)),
+						getNoteName(currentNotes.get(4)),
+						eventTick - currentTick)).append("\n");
+			}
+		}
+	}
+
+	/**
+	 * Получить код мелодии таймлайна
+	 */
+	@Override
+	public String getCode() {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(String.format("\nvoid %s() {\n", "song"));
+
+		//список включенных нот на данном этапе
+		ArrayList<Integer> currentNotes = new ArrayList<>();
+		//текущий тик (с прошлого события)
+		long currentTick = 0;
+		for (Map.Entry<Long, RawEvent> entry : events.entrySet()) {
+			long eventTick = entry.getKey().longValue();
+			appendCommand(stringBuilder, eventTick, currentTick, currentNotes);
+			currentTick = eventTick;
+			RawEvent event = entry.getValue();
+			for (RawEventTask task : event.getTasks()) {
+				int type = task.getType();
+				if (type == RawEventTask.TASK_ADD_NOTE) {
+					currentNotes.add(Integer.valueOf(task.getNote()));
+				} else if (type == RawEventTask.TASK_REMOVE_NOTE) {
+					currentNotes.remove(Integer.valueOf(task.getNote()));
+				}
+			}
+		}
+		//для того, чтобы закрыть последний элемент верно (мы пишем ноты на следующем событии, чтобы знать длительность)
+		appendCommand(stringBuilder, currentTick, currentTick, currentNotes);
+		stringBuilder.append("}");
+		return stringBuilder.toString();
 	}
 
 }
